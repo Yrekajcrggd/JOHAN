a4f381591f5fe9a55273e99bf49e3c689602a435
Yrekajcrggd <163852892+Yrekajcrggd@users.noreply.github.com>
2024-03-22 07:48:04 +0000

    Update messi.js

--- plugins/messi.js
+++ plugins/messi.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 let handler = async(m, { conn, usedPrefix, command }) => {
-let res = (await axios.get(`https://raw.githubusercontent.com/Guru322/api/Guru/BOT-JSON/Messi.json`)).data  
+let res = (await axios.get(`https://telegra.ph/file/fead512c3b76a675fb78f.jpg`)).data  
 let url = await res[Math.floor(res.length * Math.random())]
 conn.sendFile(m.chat, url, 'error.jpg', `*Messi*`, m)} 
 //conn.sendButton(m.chat, "*Messi*", author, url, [['⚽ NEXT ⚽', `${usedPrefix + command}`]], m)}
-handler.help = ['messi']
+handler.help = ['عمك']
 handler.tags = ['img']
-handler.command = /^(ميسي)$/i
+handler.command = /^(Uchiha)$/i
 export default handler
