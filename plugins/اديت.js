5cd1ef49c14b4f03e52e2b1626caf9e16a74513b
Yrekajcrggd <163852892+Yrekajcrggd@users.noreply.github.com>
2024-03-28 01:38:06 +0000

    Update اديت.js

--- plugins/اديت.js
+++ plugins/اديت.js
@@ -9,25 +9,25 @@ let handler = async (m, { conn, usedPrefix, command }) => {
   await conn.sendMessage(m.chat, { video: { url: dir[Math.floor(Math.random() * dir.length)] }, caption: global.veeeee }, { quoted: m })
 }
 
-handler.help = ['اديت']
+handler.help = ['شادو']
 handler.tags = ['anime']
-handler.command = /^(اديت)$/i
+handler.command = /^(شادو)$/i
 handler.limit = false
 
 export default handler
 
 const dir = [
 'https://telegra.ph/file/5fb7c13a4d93917f97ff3.mp4',
-'https://telegra.ph/file/2a4e007bec39cc66385b0.mp4',
-'https://telegra.ph/file/a22d5d23a85c4d7b2cdac.mp4',
-'https://telegra.ph/file/148dcadb72c631e0a9d1c.mp4',
-'https://telegra.ph/file/6699964c4f9486bafac22.mp4',
-'https://telegra.ph/file/aec768d540e249ceb0c5b.mp4',
-'https://telegra.ph/file/b2f92a40a7b869896d360.mp4',
-'https://telegra.ph/file/cd611bb1e76ceac182de8.mp4',
-'https://telegra.ph/file/0c4046c6477431bbed40d.mp4',
-'https://telegra.ph/file/d84e53e96fb44ec4cbd23.mp4',
-'https://telegra.ph/file/1286e1bf83c9901308cd8.mp4',
+'https://telegra.ph/file/22ecc343f08752befd263.mp4',
+'https://telegra.ph/file/ceccbf7b0430ea3e540df.mp4',
+'https://telegra.ph/file/c41394beb1d1dc75c21b2.mp4',
+'https://telegra.ph/file/7e83a6c0581f1ac6fc896.mp4',
+'https://telegra.ph/file/6459a5571aa37b3ff4d09.mp4',
+'https://telegra.ph/file/9de72c2d414605c800898.mp4',
+'https://telegra.ph/file/c6c9f0e8d39fd59e386dd.mp4',
+'https://telegra.ph/file/a631fa901c8d85c9c9985.mp4https://telegra.ph/file/c6c9f0e8d39fd59e386dd.mp4',
+'https://telegra.ph/file/46477c73c9a14ed3726ea.mp4',
+'https://telegra.ph/file/e1204862b247b4d183595.mp4',
 
 
 
